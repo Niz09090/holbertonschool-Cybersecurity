2e411cd5114c0c9a443ae99d6ab95492deb941fe
Niz09090 <amanqa6@gmail.com>
2026-05-28 17:15:25 -0400

    test

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -c "import ipaddress; print(ipaddress.IPv4Network('0.0.0.0/$1').netmask)"
+mask=$(( 0xFFFFFFFF << (32 - $1) & 0xFFFFFFFF )); printf '%d.%d.%d.%d\n' $(( (mask>>24)&255 )) $(( (mask>>16)&255 )) $(( (mask>>8)&255 )) $(( mask&255 ))
