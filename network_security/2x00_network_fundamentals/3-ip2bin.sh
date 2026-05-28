10578f0516df2f7fbf8c55cf2d8d0251eec24672
Niz09090 <amanqa6@gmail.com>
2026-05-28 17:14:17 -0400

    test

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-IFS='.' read -r o1 o2 o3 o4 <<< "$1"
-printf '%08d.%08d.%08d.%08d\n' $(echo "ibase=10;obase=2;$o1" | bc) $(echo "ibase=10;obase=2;$o2" | bc) $(echo "ibase=10;obase=2;$o3" | bc) $(echo "ibase=10;obase=2;$o4" | bc)
+IFS='.' read -r o1 o2 o3 o4 <<< "$1"; printf '%08d.%08d.%08d.%08d\n' $(echo "ibase=10;obase=2;$o1" | bc) $(echo "ibase=10;obase=2;$o2" | bc) $(echo "ibase=10;obase=2;$o3" | bc) $(echo "ibase=10;obase=2;$o4" | bc)
